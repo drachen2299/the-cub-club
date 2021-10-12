409205a955976103958f8e60e07bc328e2a398c8
rachelperez <rlb2rlp@gmail.com>
2021-10-12 14:52:11 -0700

    remove backend bear collection files

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -13,7 +13,7 @@ export const defaultRoute = async () => {
 
 export const createBear = async (userId, newBear) => {
   try {
-    const response = await axios.post(`${apiURL}/user/new-bear/${userId}`, newBear);
+    const response = await axios.post(`${apiURL}/usetr/new-bear/${userId}`, newBear);
     return response.data;
   } catch (e) {
     console.error(e.message);
