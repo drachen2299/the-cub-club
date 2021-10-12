fdada568bd17c843dae993d3f823d4fc4c2f89ce
nateshim <nateshim@berkeley.edu>
2021-10-12 11:18:01 -0700

    mongoose auth model seeds

--- seeds/users.js
+++ seeds/users.js
@@ -1,6 +1,7 @@
 const db = require("../db");
 const User = require("../models/user");
 const Bear = require("../models/bear");
+const {hashPassword} = require("../utils");
 
 const createUser = async () => {
     const bears = await Bear.findOne({ color: "panda" });
@@ -8,16 +9,19 @@ const createUser = async () => {
     {
         username: "beeboop12",
         email: "beeb@email.com",
+        password: hashPassword("testpassword"),
         bear: bears._id
     },
     {
         username: "derpherder31",
         email: "derp31@email.com",
+        password: hashPassword("testpassword"),
         bear: bears._id
     },
     {
         username: "herrderr543",
         email: "heer543@email.com",
+        password: hashPassword("testpassword"),
         bear: bears._id
     }
 ]
