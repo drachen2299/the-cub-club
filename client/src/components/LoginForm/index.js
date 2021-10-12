b03188d3a5f9d64286f87358f9ac0c06e3e0df08
rachelperez <rlb2rlp@gmail.com>
2021-10-12 11:31:06 -0700

    create login form

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -1,8 +1,26 @@
+import { useState } from "react";
+
 const LoginForm = () => {
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPw, setConfirmPw] = useState("");
+
   return (
-    <div>
-      This is the log in form
-    </div>
+    <section>
+      <form >
+        <label htmlFor="username">Username:</label>
+        <input id="username" type="text" />
+        <label htmlFor="email">Email:</label>
+        <input id="email" type="text" />
+        <label htmlFor="password">Password:</label>
+        <input id="password" type="text" />
+        <label htmlFor="confirmPassword">Confirm Password:</label>
+        <input id="confirmPassword" type="text" />
+        <button type="submit">Log in</button>
+      </form>
+    </section>
   );
 };
 
