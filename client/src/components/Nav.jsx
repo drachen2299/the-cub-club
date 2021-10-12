ae8d280965ebff41a191392e08580214a3076ce4
nateshim <nateshim@berkeley.edu>
2021-10-12 10:18:14 -0700

    editing bugs

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -4,7 +4,7 @@ const Nav = () => {
   return (
     <nav>
       <Link to="/">Home</Link>
-      <Link to="/create-avatar/:id">Create Avatar</Link>
+      <Link to="/create-avatar:id">Create Avatar</Link>
       <Link to="sign-up">Sign up</Link>
       <Link to="log-in">Log in</Link>
       <Link to="/game">Game</Link>
--- client/src/screens/Home/index.jsx
+++ client/src/screens/Home/index.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <section>
-            
+      This is home
     </section>
   );
 };
