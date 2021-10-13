47fec6338184b2bfe3ae385e6f659d9e32802541
nateshim <nateshim@berkeley.edu>
2021-10-13 13:57:11 -0700

    styling Game Modal

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -55,8 +55,8 @@ const Modal = (props) => {
                 <h3>{recipient?.username}</h3>
               </div>
               <form className="MessageForm" onSubmit={handleSubmit}>
-                <label className="Header" htmlFor="message">
-                  <h2>Message:</h2>
+                <label htmlFor="message">
+                  <h2 className="Header">Message:</h2>
                 </label>
                 <input id="message" type="text" value={letter} onChange={(e) => setLetter(e.target.value)}/>
                 <button type="submit">Send!</button>
