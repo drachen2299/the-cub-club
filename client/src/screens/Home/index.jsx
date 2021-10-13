ab68da8b6f7e26a980a28789b96fb18711097728
drachen2299 <benlowry55@gmail.com>
2021-10-13 17:37:21 -0600

    added useHistory to avatar creation

--- client/src/screens/Home/index.jsx
+++ client/src/screens/Home/index.jsx
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <section>
       <h2>Welcome!</h2>
       <h2>**Instructions will go here**</h2>
-      <h2>Log in or sign up</h2>
+      <Link to="/log-in">
+        <h2>Login</h2>
+      </Link>
+      <Link to="/account-creation">
+        <h2>Sign Up</h2>
+      </Link>
     </section>
   );
 };
