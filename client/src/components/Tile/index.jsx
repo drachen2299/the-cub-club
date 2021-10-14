484c1204eccac6020d3f25acd106dda5041bc4fb
nateshim <nateshim@berkeley.edu>
2021-10-14 12:10:42 -0700

    renders proper bear based on user account creation

--- client/src/components/Tile/index.jsx
+++ client/src/components/Tile/index.jsx
@@ -4,7 +4,8 @@ const Tile = (props) => {
         {props.tiles.map((tile, i) => (
         <div onClick={() => {
             if (Math.abs(props.selectedTile[1] - tile.x) + Math.abs(props.selectedTile[0] - tile.y) === 1 && tile.contents === "mailbox") props.setShowModal((curr) => !curr)
-        }} className={`${props.selectedTile[1] === tile.x && props.selectedTile[0] === tile.y && "selected"} ${props.SelectedTile}`}></div>
+        }} className={
+            `${props.selectedTile[1] === tile.x && props.selectedTile[0] === tile.y && "selected" && props.bear.fur} ${props.SelectedTile}`}></div>
         ))}
         </>
     );
--- client/src/screens/Game/index.jsx
+++ client/src/screens/Game/index.jsx
@@ -10,6 +10,7 @@ const Game = (props) => {
   const [showModal, setShowModal] = useState(false);
   const [tiles, setTiles] = useState(createEmptyBoard(10,2));
   const [{ selectedTile, canKeyPress }, dispatch] = useKeyPress([0,0]);
+
   useEffect(() => {
     window.addEventListener("keydown", (e) => dispatch({type: e.keyCode}));
     return () => {
@@ -32,7 +33,7 @@ const Game = (props) => {
         <Modal showModal={showModal} setShowModal={setShowModal} user={props.user} />
         <div className="game-board">
           <div className="grid-board">
-            <Tile setShowModal={setShowModal} selectedTile={selectedTile} tiles={tiles} />
+            <Tile bear={props.user.bear} setShowModal={setShowModal} selectedTile={selectedTile} tiles={tiles} />
           </div>
         </div>
       </div>
