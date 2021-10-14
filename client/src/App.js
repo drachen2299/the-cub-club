6bb81ee613ca503578015906c747d1f0c1c738a4
drachen2299 <benlowry55@gmail.com>
2021-10-14 17:27:57 -0600

    -added user routes and aunthentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,29 @@
 import {Switch, Route } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Home from './screens/Home';
 import Register from './screens/Register';
+import Avatar from "./components/Avatar";
+import { verifyUser } from "./services";
 import Login from './screens/Login';
 import Game from './screens/Game';
+import { useHistory } from "react-router-dom";
 import './sass/input.scss';
 
 function App() {
   const [user, setUser] = useState(null);
-
+  const history = useHistory();
+  useEffect(() => {
+    const fetchVerifyUser = async () => {
+      
+      const verifiedUser = await verifyUser();
+      if (!verifiedUser) {
+        history.push("/");
+      } else {
+        setUser(verifiedUser);
+      }
+    }
+    fetchVerifyUser();
+  }, [history])
   return (
     <div className="App">
       <Switch>
