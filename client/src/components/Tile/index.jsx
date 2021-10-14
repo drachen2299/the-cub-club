dd6c23bd5668c87d7bca958ea3e15cbd732a385f
nateshim <nateshim@berkeley.edu>
2021-10-14 12:56:04 -0700

    fixing error

--- client/src/components/Tile/index.jsx
+++ client/src/components/Tile/index.jsx
@@ -4,7 +4,7 @@ const Tile = (props) => {
         {props.tiles.map((tile, i) => (
         <div onClick={() => {
             if (Math.abs(props.selectedTile[1] - tile.x) + Math.abs(props.selectedTile[0] - tile.y) === 1 && tile.contents === "mailbox") props.setShowModal((curr) => !curr)
-        }} className={`${tile.contents === "mailbox" ? "mailbox" : null} ${props.selectedTile[1] === tile.x && props.selectedTile[0] === tile.y && "selected" && props.bear.fur} ${props.SelectedTile}`}></div>
+        }} className={`${props.selectedTile[1] === tile.x && props.selectedTile[0] === tile.y && "selected" && props.bear.fur} ${props.SelectedTile} ${tile.contents === "mailbox" ? "mailbox" : null}`}></div>
         ))}
         </>
     );
