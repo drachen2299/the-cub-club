1d23b806a95f2f328be080f0fa73f756ad87a04b
drachen2299 <benlowry55@gmail.com>
2021-10-15 12:47:07 -0600

    logout functionality working

--- client/src/App.js
+++ client/src/App.js
@@ -64,7 +64,7 @@ function App() {
           </Route>
           <Route exact path="/game">
             {user ? (
-              <Game user={user} /> 
+              <Game user={user} setUser={setUser}/> 
             ) : null}
             </Route>
         </main>
