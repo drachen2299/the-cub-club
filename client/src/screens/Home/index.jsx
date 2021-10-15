36335dec8ea5f11ab1270a2e7b819d8c249f607b
rachelperez <rlb2rlp@gmail.com>
2021-10-14 21:56:36 -0700

    initial nav styling

--- client/src/screens/Home/index.jsx
+++ client/src/screens/Home/index.jsx
@@ -8,10 +8,10 @@ const Home = () => {
       <h2>Welcome!</h2>
       <h2>**Instructions will go here**</h2>
       <Link to="/log-in">
-        <h2>Login</h2>
+        <button>Login</button>
       </Link>
       <Link to="/account-creation">
-        <h2>Sign Up</h2>
+        <button>Sign Up</button>
       </Link>
     </section>
   );
