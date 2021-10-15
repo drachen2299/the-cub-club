3ae729932eea0ffef089d8e9064ca402368b6dce
drachen2299 <benlowry55@gmail.com>
2021-10-15 17:45:10 -0600

    more template styling

--- client/src/screens/Home/index.jsx
+++ client/src/screens/Home/index.jsx
@@ -2,17 +2,19 @@ import { Link } from "react-router-dom";
 import homeImage from '../../assets/home-asset.png'
 const Home = () => {
   return (
-    <section className="home">
-      <img id='home-img' src={homeImage} alt="Tofu pointing at a sign"/>
-      <h1 className="home-logo">THE CUB CLUB</h1>
-      <h2 className="home-heading">Welcome!</h2>
-      <h2 className="home-textfield">**Instructions will go here**</h2>
-      <Link to="/log-in">
+    <section className="screen">
+      <img id='main-img' src={homeImage} alt="Tofu pointing at a sign"/>
+      <div id="home-screen-contents">
+      <h1 className="logo">THE CUB CLUB</h1>
+      <h2 className="heading">Welcome!</h2>
+      <h2 className="textfield">**Instructions will go here**</h2>
+      <Link className="btn-1-home" to="/log-in">
         <button className="home-login-button">Login</button>
       </Link>
-      <Link to="/account-creation">
+      <Link className="btn-2-home" to="/account-creation">
         <button className="home-signup-button">Sign Up</button>
       </Link>
+      </div>
     </section>
   );
 };
