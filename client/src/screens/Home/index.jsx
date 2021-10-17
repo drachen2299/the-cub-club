4a8c74f9add860f2db8a46d0df8f34a5e42c81ed
drachen2299 <benlowry55@gmail.com>
2021-10-17 17:28:25 -0600

    -fixing content to fix mobile and desktop

--- client/src/screens/Home/index.jsx
+++ client/src/screens/Home/index.jsx
@@ -7,14 +7,16 @@ const Home = () => {
 
       <div id="home-screen-contents">
         <h1 className="logo">THE CUB CLUB</h1>
-        <h2 className="heading">Welcome!</h2>
-        <h2 className="textfield">**Instructions will go here**</h2>
-        <Link className="btn-1-home" to="/log-in">
-          <button className="home-login-button">Login</button>
-        </Link>
-        <Link className="btn-2-home" to="/account-creation">
-          <button className="home-signup-button">Sign Up</button>
-        </Link>
+        <div className="inner-content">
+          <h2 className="heading">Welcome!</h2>
+          <h2 className="textfield">**Instructions will go here**</h2>
+          <Link className="btn-1-home" to="/log-in">
+            <button className="home-login-button">Login</button>
+          </Link>
+          <Link className="btn-2-home" to="/account-creation">
+            <button className="home-signup-button">Sign Up</button>
+          </Link>
+        </div>
       </div>
     </section>
   );
